054af47568adf4217adf303c71caec845b6a820a
Lakshman Kumar K N <lkumar93@umd.edu>
2016-09-30 23:34:04 -0400

    Add Camera Calibration Parameters

---
+++ src/deep_learning_crazyflie/include/deep_learning_crazyflie/camera_parameters.h
@@ -0,0 +1,8 @@
+#define CAMERA_PARAMETERS_H_
+
+double FocalLength_X = 379.0506;
+double FocalLength_Y = 366.6033;
+double PrincipalPoint_X = 363.9286;
+double PrincipalPoint_Y = 290.6407;
+double Distortion[5] = {0.1718, 0.1043, 0.0029, 0.0064,-0.3638};
+
