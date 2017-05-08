fc40ad10e45f3d79099a3e69cba992b9cce8035d
lkumar93 <lkumar93@umd.edu>
2017-05-08 18:16:54 -0400

    Add calibration parameters and images for crazyflie cameras

---
+++ src/deep_learning_crazyflie/include/deep_learning_crazyflie/bottom_camera_parameters.h
@@ -0,0 +1,11 @@
+#define TOP_CAMERA_PARAMETERS_H_
+
+double FocalLength_X_Top = 668.1563;
+double FocalLength_Y_Top = 545.1772;
+double PrincipalPoint_X_Top = 985.7908;
+double PrincipalPoint_Y_Top = 617.8635;
+double Distortion_Top[5] = {0.20251939, -0.24466239, -0.00098169, 0.00123158,0.06783239};
+
+float calibration_square_size = 2.5 //cm
+float calibration_pattern_size = {9,7} //rows X cols
+
